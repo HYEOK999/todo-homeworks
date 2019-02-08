232b33bfa31d38ff06d8efc1ef8550aea56191d9
hyeok <iasg2004@naver.com>
2019-02-08 17:10:12 +0900

    header 태그 추가 및 할일 값을 넣어놓은 scasular data 생성

--- 김준혁/src/App.vue
+++ 김준혁/src/App.vue
@@ -1,13 +1,43 @@
 <template>
-  
+  <div id="app">
+    <TodoHeader></TodoHeader>
+  </div>
 </template>
 
 <script>
-export default {
+import TodoHeader from './components/TodoHeader.vue'
 
+export default {
+  data: function(){
+    return{
+      scasular: []
+  }
+},
+  methods: {
+  },
+  components: {
+    'TodoHeader' : TodoHeader,
+  },
 }
 </script>
 
 <style>
-
+ body {
+    text-align: center;
+    background-color: rgb(247, 247, 255); 
+    /*
+    text-align -> 중앙정렬
+    background-color -> 화면 전체 배경색 
+    */
+  }
+  input {
+    border-style: groove;
+    width: 200px;
+  }
+  button {
+    border-style: groove;
+  }
+  .shadow {
+    box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03)
+  }
 </style>
---
+++ 김준혁/src/components/TodoHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <header>
+        <h1>Todo App Project!!</h1>
+    </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    h1{
+        color: #11808fcc;
+        font-weight: 1000;
+        margin: 2.5rem 0 1.5rem;
+    }
+</style>
